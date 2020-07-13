7c89d5854187c2574653c3fce7a8ca4834999f8b
unknown <benfarhatamani@gmail.com>
2020-07-13 16:39:47 +0100

    few changes

--- models/User.js
+++ models/User.js
@@ -5,11 +5,11 @@ const Tag = mongoose.model(
     new mongoose.Schema({
         name : { type : String , unique : false, required : true },
         last_name : { type : String , unique : false, required : true },
-        birth : { type : Date , unique : false, required : false },
+        birth : { type : String , unique : false, required : false },
         password : { type : String , unique : false, required : true },
         email : { type : String , unique : true, required : true },
         inscription_number : { type : String , unique : false, required : true },
-        grade : { type : Number , unique : false, required : true },
+        grade : { type : String , unique : false, required : true },
         role : { type : String , unique : false, required : true },
         join_date : { type : Date , unique : false, default : Date.now },
         active : { type : Number , unique : false, default : 0 },
